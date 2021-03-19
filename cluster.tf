bf066775628f39ef5f44cb6d179e32ff6d45e554
Gaston Festari <cilindrox@gmail.com>
2021-03-19 15:03:16 -0300

    feat: parameterize cluster security group name

--- cluster.tf
+++ cluster.tf
@@ -83,6 +83,7 @@ resource "null_resource" "wait_for_cluster" {
 
 resource "aws_security_group" "cluster" {
   count       = var.cluster_create_security_group && var.create_eks ? 1 : 0
+  name        = var.cluster_security_group_name != "" ? var.cluster_security_group_name : null
   name_prefix = var.cluster_name
   description = var.cluster_security_group_description
   vpc_id      = var.vpc_id
